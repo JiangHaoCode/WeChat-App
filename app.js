2a26bf7480b7eda8e395c50addcf31bd2374f687
jianghao <jianghao@haplox.com>
2019-08-25 16:37:44 +0800

    First

---
+++ app.js
@@ -0,0 +1,21 @@
+App({
+  // app.js 生命周期
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  },
+  onError: function(err) {
+    console.log("app.js ---onError---" + err)
+  },
+
+
+  globalData: {
+    hasLogin: false,
+    message: 'message to ago'
+  }
+})
---
+++ utils/util.js
@@ -0,0 +1,31 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+const phoneTest = n => {
+  const regex = /^1[3456789]\d{9}$/
+  return regex.test(n)
+}
+
+const emailTest = n => {
+  const regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+  return regex.test(n)
+}
+
+module.exports = {
+  formatTime: formatTime,
+  phoneTest,
+  emailTest
+}
